f482185208f366327f810994840f1c33977fa090
Nicolas Burel <nicolas.burel@fr.ey.com>
2017-03-05 21:05:37 +0100

    Typo

--- trumpbot.py
+++ trumpbot.py
@@ -4,7 +4,7 @@ from warnings import warn
 
 import numpy as np
 import pandas as pd
-from keras.layers import Dense, Activation, LSTM, Dropout, GRU
+from keras.layers import Dense, Activation, Dropout, GRU
 from keras.models import Sequential
 
 # data import
